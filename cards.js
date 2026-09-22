bd5c958d9faa60809803044beb83bc84d7ae7b54
JiranuwatRuengdet <jiranuwat094@gmail.com>
2026-09-22 09:35:01 +0700

    Update cards.js

---
+++ cards.js
@@ -0,0 +1,5 @@
+const cards = [
+  { name: "Blue-Eyes White Dragon", type: "Monster", atk: 3000, def: 2500, image: "images/blue-eyes.jpg" },
+  { name: "Dark Magician", type: "Monster", atk: 2500, def: 2100, image: "images/dark-magician.jpg" },
+  // เพิ่มการ์ดใหม่ต่อท้ายเรื่อยๆ
+];
